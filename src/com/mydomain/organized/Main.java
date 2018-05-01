7b6cb2d305d3f6e515158f69662560c1e7576e91
salysam <katty.yumei@gmail.com>
2018-05-01 15:03:24 +0100

    Add Unit Test

--- src/com/mydomain/organized/Main.java
+++ src/com/mydomain/organized/Main.java
@@ -5,12 +5,6 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        TinyUrl testUrl = new TinyUrl();
-        String result1 = testUrl.encode("http://git-extensions-documentation.readthedocs.io/en/latest/getting_started.html");
-        System.out.println(result1);
-
-        String result2 = testUrl.decode("www.tinyurl.ma/3e5728a6");
-        System.out.println(result2);
 
     }
 }
---
+++ src/com/mydomain/organized/TinyUrlTest.java
@@ -0,0 +1,21 @@
+package com.mydomain.organized;
+
+import junit.framework.TestCase;
+
+public class TinyUrlTest extends TestCase {
+
+    TinyUrl testUrl = new TinyUrl();
+
+    public void testEncode() {
+        String actualUrl = "http://git-extensions-documentation.readthedocs.io/en/latest/getting_started.html";
+        String expectUrl = "www.tinyurl.ma/3e5728a6";
+        String result = testUrl.encode(actualUrl);
+        assertEquals(expectUrl, result);
+    }
+
+    public void testDecode() {
+        String url = "http://git-extensions-documentation.readthedocs.io/en/latest/getting_started.html";
+        String result = testUrl.decode(testUrl.encode(url));
+        assertEquals(url, result);
+    }
+}
