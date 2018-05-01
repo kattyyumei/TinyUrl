8e283c62cffec9c9536b9cd5461517632c917d60
salysam <katty.yumei@gmail.com>
2018-05-01 14:16:58 +0100

    Initial commit

---
+++ src/com/mydomain/organized/TinyUrl.java
@@ -0,0 +1,26 @@
+package com.mydomain.organized;
+
+import java.util.HashMap;
+
+public class TinyUrl {
+    private HashMap<String, String> urls = new HashMap<>();
+
+    public String encode(String url){
+        String tinyUrl;
+        tinyUrl = "www.tinyurl.ma/";
+        String uniqueKey = Integer.toHexString(url.hashCode());
+        tinyUrl += uniqueKey;
+        urls.put(tinyUrl, url);
+        return  tinyUrl;
+    }
+
+    public  String decode(String tinyurl){
+        String notFound = "Your tiny url is not found";
+        String url = urls.get(tinyurl);
+        if (url == null){
+            return  notFound;
+        }
+        else
+            return url;
+    }
+}
